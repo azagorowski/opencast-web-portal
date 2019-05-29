eeeffdca5e68d896676b1c2dd794a7eda9cd7f38
azagorowski <37112877+azagorowski@users.noreply.github.com>
2019-05-29 19:02:21 +0200

    Friendly URL correction

--- src/index.php
+++ src/index.php
@@ -46,19 +46,19 @@
 	</section>
 	<!-- End of banner (section) -->
 	<section class="small-section-4" id="login">
-		<a href="login.php"><td><i class="icon-login"></i></td>
+		<a href="login"><td><i class="icon-login"></i></td>
 		<h3>Login</h3></a>
 	</section>
 	<section class="small-section-4" id="register">
-		<a href="register.php"><td><i class="icon-user-plus"></i></td>
+		<a href="register"><td><i class="icon-user-plus"></i></td>
 		<h3>Rejestracja</h3></a>
 	</section>
 	<section class="small-section-4" id="wbase">
-		<a href="wbase.php"><td><i class="icon-search"></i></td>
+		<a href="miningpedia"><td><i class="icon-search"></i></td>
 		<h3>Baza Wiedzy</h3></a>
 	</section>
 	<section class="small-section-4" id="games">
-		<a href="games.php"><td><i class="icon-gamepad"></i></td>
+		<a href="games"><td><i class="icon-gamepad"></i></td>
 		<h3>Gry</h3></a>
 	</section>
 <!-- End of small sections -->
